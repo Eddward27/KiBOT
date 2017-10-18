4a9fb52aab28daadead7b0b9d4275f7491d2eeb6
Matías Eduardo Allende Pino <matias.allende.p@mail.pucv.cl>
2017-10-17 23:06:52 -0300

    +[kick, prefijo, say y sayd], uso de config.json
    Se agregó el uso de /config/config.json
    -!kick agregado
    -!id agregado
    -!say y !sayd agregado
    -!prefijo agregado
    -Cambió como se lee el comando con 'command'

--- kibot.js
+++ kibot.js
@@ -1,11 +1,11 @@
-//Referencia al paquete Discord
+//Referencias a Packages
 const Discord = require('discord.js');	//Uso de libreria 'discord.js'
+const fs = require('fs');   //Uso de fs
+
 const bot = new Discord.Client();	//Instancia de bot
 
 //Variables para el bot
-const prefix = '!';	//Prefijo usado para activar comandos de bot
-const token = 'TOKEN'	//Token secreto para usar bot, escondido por razones de seguridad
-const avatarURLkBOT = 'https://cdn.discordapp.com/attachments/368996103917666305/369657756304801802/me2.png';	//URL de avatar para KiBOT, por el mismisimo Kibo
+const config = require("./config/config.json");
 
 // Functions
 function hook(channel, title, message, color, avatar) {	//Funcion que crea los webhooks '!hook'
@@ -13,7 +13,7 @@ function hook(channel, title, message, color, avatar) {	//Funcion que crea los w
     if (!title) return console.log('Titulo no especificado.');
     if (!message) return console.log('Mesnsaje no especificado.');
     if (!color) color = 'd9a744';
-    if (!avatar) avatar = avatarURLkBOT;
+    if (!avatar) avatar = config.botAvatar;
 
     color = color.replace(/\s/g, '');	//Elimina espacios que puedan quedar en color y avatar
     avatar = avatar.replace(/\s/g, '');
@@ -23,7 +23,7 @@ function hook(channel, title, message, color, avatar) {	//Funcion que crea los w
             let foundHook = webhook.find('name', 'Webhook');
 
             if (!foundHook) {
-                channel.createWebhook('Webhook', avatarURLkBOT)
+                channel.createWebhook('Webhook', config.botAvatar)
                     .then(webhook => {
                         webhook.send('', {
                             "username": title,
@@ -60,17 +60,41 @@ bot.on('message', message => {
 	//Variables
 	let msg = message.content.toUpperCase();	//Mensaje en mayúsculas
 	let sender = message.author;	//Autor del mensaje que activo el comando
-	let cont = message.content.slice(prefix.lenght).split(" "); //Corta el prefijo del mensaje y lo separa por espacios, guardados en un arreglo
-	let args = cont.slice(1); //Corta el comando dejando solo los argumentos
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toUpperCase();
+	//let cont = message.content.slice(config.prefix.lenght).trim().split(/ +/g); //Corta el prefijo del mensaje y lo separa por espacios, guardados en un arreglo
+	//let args = cont.slice(1); //Corta el comando dejando solo los argumentos
 
-    if(sender.bot){ //Ignora los mensajes de cualquier BOT
+    if(sender.bot || !message.content.startsWith(config.prefix)){ //Ignora los mensajes de cualquier BOT y también ignora si no comienza con el prefijo
         return;
     }
+
+    //Comandos de modificación
+    if(command === 'PREFIJO'){
+        if(msg === config.prefix + 'PREFIJO'){  //Si el comando se envió sin argumentos, se retorna el prefijo actual
+            message.channel.send('Prefijo actual: \''+config.prefix+'\'')
+            return;
+        }
+        if(sender.id !== config.ownerID) return message.channel.send('**No tienes permisos para cambiar este valor**'); //Verifica si el autor del mensaje es el dueño
+        /*if(args[0].lenght !== 1){   //Verifica si el nuevo prefijo es de 1 caráter de largo
+            message.channel.send('**Intenta con un prefijo de 1 carácter :baby_chick:**');
+            return;
+        }*/
+        let nuevoPrefijo = args[0]; //Cambia el prefijo en la memoria
+
+        console.log('Cambio de prefijo: \''+config.prefix+'\' a \''+nuevoPrefijo+'\'...')   //Avisa por consola el cambio de prefijo
+
+        config.prefix = nuevoPrefijo;   //Guarda la configuración en la memoria
+        fs.writeFile('./config/config.json', JSON.stringify(config), (err) => console.error);   //Modifica el archivo '/config/config.json'
+
+        message.channel.send('**@everyone Nuevo prefijo: \''+config.prefix+'\'**');   //Avisa por Discord
+    }
+    //Comandos de modificación-END
 
 	//Comandos
 
 	//Ping-Pong!
-	if(msg === prefix + 'PING'){
+	if(command === 'PING'){
         let embed = new Discord.RichEmbed()
             .setColor('FF0000')
             .setDescription(':ping_pong: Pong! - '+bot.ping+ 'ms!')
@@ -79,7 +103,7 @@ bot.on('message', message => {
 
 /*
     //Uptime
-    if(msg === prefix + 'UPTIME'){
+    if(command === 'UPTIME'){
         let embed = new Discord.RichEmbed()
             .setColor('FF0000')
             .setDescription('Uptime: '+bot.uptime)
@@ -87,13 +111,13 @@ bot.on('message', message => {
 	}
 */
 /*
-    if(msg === prefix + 'INVITE'){
+    if(command === 'INVITE'){
         bot.generateInvite(['ADMINISTRATOR'])
             .then(link => {
                 console.log(`Link de invitación generado: ${link}`);
         let embed = new Discord.RichEmbed()
             .setTitle('KiBOT: Link de invitación')
-            .setImage(avatarURLkBOT)
+            .setImage(config.botAvatar)
             .setColor(0x00AE86)
             .setURL(link)
         message.channel.send({ embed });
@@ -102,7 +126,7 @@ bot.on('message', message => {
 */
 
 	//Purge
-	if(msg.startsWith(prefix + 'PURGE')){
+	if(command === 'PURGE'){
 		async function purge() {
 			message.delete(); //Se borra el mensaje que activo el comando para que no interfiera con el borrado
 
@@ -114,11 +138,11 @@ bot.on('message', message => {
 
 			//Verifica si el argumento es un número
 			if(isNaN(args[0])){
-				message.channel.send('Usa un número como argumento!\nUso: '+prefix+'purge <cantidad>');
+				message.channel.send('Usa un número como argumento!\nUso: '+config.prefix+'purge <cantidad>');
 				return;
 			}
 
-			const fetched = await message.channel.fetchMessages({limit: args[0]}); //Toma los ultimos 'args[0]' mensajes en el canal
+			const fetched = await message.channel.fetchMessages({limit: args[0]}); //Toma los ultimos ${args[0]} mensajes en el canal
 			console.log(args[0]+' mensajes pedidos a borrar, '+fetched.size + ' mensajes encontrados, borrando...');	//Detalles por consola
 
 			//Borrando
@@ -130,26 +154,26 @@ bot.on('message', message => {
 	}
 
     //Webhook
-	if (msg.startsWith(prefix + 'HOOK')) {
+	if (command === 'HOOK') {
         message.delete();	//Elimina el mensaje que origino el comando
 
-        if (msg === prefix + 'HOOK') {	//!hook sin argumentos
-            return hook(message.channel, 'Uso de Hook', sender+`\n${prefix}hook <título>, <mensaje>, [color hexadecimal], [avatarURL]\n\n**<> es requerido\n[] es opcional**`,'FC8469','https://cdn0.iconfinder.com/data/icons/user-collection-4/512/user_help-128.png'); // Remeber that \n means new line. This is also using a custom HEX id, and an image.
+        if (msg === config.prefix + 'HOOK') {	//!hook sin argumentos
+            return hook(message.channel, 'Uso de Hook', sender+`\n${config.prefix}hook <título>, <mensaje>, [color hexadecimal], [avatarURL]\n\n**<> es requerido\n[] es opcional**`,'FC8469','https://cdn0.iconfinder.com/data/icons/user-collection-4/512/user_help-128.png'); // Remeber that \n means new line. This is also using a custom HEX id, and an image.
         }
 
-        let hookArgs = message.content.slice(prefix.length + 4).split(",");	//Separa argumentos en arreglo, argumentos enviados separados por comas ','
+        let hookArgs = message.content.slice(config.prefix.length + 4).split(",");	//Separa argumentos en arreglo, argumentos enviados separados por comas ','
         hook(message.channel, hookArgs[0], hookArgs[1], hookArgs[2], hookArgs[3]);
     }
 
     //Dado
-	if(msg === prefix + 'DADO'){	//Dado, aleatorio de [1,6], hace uso de webhook (!hook)
+	if(command === 'DADO'){	//Dado, aleatorio de [1,6], hace uso de webhook (!hook)
 		message.delete();
 		var dado = Math.floor((Math.random() * 6) + 1);
 		hook(message.channel, 'Dado', sender+' Haz lanzado un '+dado+'!','308030','https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/die-128.png');
 	}
 
     //Avatar
-	if(msg === prefix + 'AVATAR'){	//Muestra el avatar el autor del mensaje
+	if(command === 'AVATAR'){	//Muestra el avatar el autor del mensaje
         const embed = new Discord.RichEmbed()
             .setAuthor('Avatar')
             .setDescription(sender)
@@ -159,7 +183,35 @@ bot.on('message', message => {
         message.channel.send({embed});
 	}
 
-    /////////
+    //ID
+    if(command === 'ID'){
+        message.channel.send(sender+' tu ID de Discord es: \''+sender.id+'\'');
+    }
+
+    //Kick
+    if(command === 'KICK'){
+        if(msg === config.prefix + 'KICK'){
+            hook(message.channel,'Uso de Kick', config.prefix+'kick <usuario>','EEFF44','https://cdn2.iconfinder.com/data/icons/human-2-1/48/90-512.png');
+            return;
+        }
+        let kickea2 = message.mentions.members.first();
+        let razon = args.slice(1).join(" ");
+        message.channel.send(kickea2+' KICKEA2!!!');
+        kickea2.kick(razon);
+    }
+
+    //Say
+    if(command === 'SAY'){
+        let texto = args.join(" ");
+        message.channel.send(texto);
+    }
+
+    //Sayd
+    if(command === 'SAYD'){
+        let texto = args.join(" ");
+        message.delete();
+        message.channel.send(texto);
+    }
 });
 
 //Evento Listener: ready
@@ -167,16 +219,16 @@ bot.on('ready', () => {
 	console.log('KiBOT IS ALIVE!!!');	//Avisa que el bot está listo por consola
 
 //Bot Status
-bot.user.setStatus('Online');    //Online, Idle, Invisible, dnd
+bot.user.setStatus('online');    //Online, Idle, Invisible, dnd ----------NO PARECE FUNCIONAR!!
 
 //Jugando, Streaming - SOLO UNO DE LOS DOS PUEDE USARSE A LA VEZ!!
     //Jugando
-bot.user.setGame('First Steps!');
+bot.user.setGame('with my code!');
     //Streaming
 //bot.user.setGame('nombre del stream', 'link twitch.tv o hitbox.tv');
 });
 
 //Login
-bot.login(token);	//Login de bot a Discord
+bot.login(config.token);	//Login de bot a Discord
 
 /////////////////////////////////////////////////////////////
