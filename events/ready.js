d43136a5290624ad3fb3302ed5350baee0f5d260
Matías Eduardo Allende Pino <matias.allende.p@mail.pucv.cl>
2018-03-07 21:19:56 -0300

    Update ready.js
    Linea para agregar un status al bot

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,15 @@
 const chalk = require('chalk');
 module.exports = client => { // eslint-disable-line no-unused-vars
+    
+    var streamSettings = {
+        name: 'Stream Pulento',
+        url: 'https://www.twitch.tv/elwarizapo',
+        type: 1
+    };
+    
+    client.user.setStatus('online'); // online, idle, invisible, dnd
+    
+    client.user.setActivity('With my code');
+    
     console.log(chalk.bgGreen.black('ESTA VIVO!!!!!!!!!!!!!'));
 };
