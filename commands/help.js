a146d7a2129390135546fc6e02a9f72dec164b63
Matías Eduardo Allende Pino <matias.allende.p@mail.pucv.cl>
2018-03-22 20:45:29 -0300

    Ahora 'help <comando>' muestra los alias que tiene ese comando

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,12 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`= Lista de comandos =\n\n[Usa ${settings.prefix}help <commandname> para más detalles]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
+    message.channel.send(`= Lista de comandos =\n\n[Usa ${settings.prefix}help <comando> para más detalles y aliases]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage::${command.help.usage}`, {code:'asciidoc'});
+      message.channel.send(`= ${command.help.name} = \n${command.help.description}\nUso: ${command.help.usage}\nAliases: ${command.conf.aliases}`, {code:'asciidoc'});
     }
   }
 };
