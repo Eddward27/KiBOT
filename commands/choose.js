a2adac4033cf4ac606d10dfe9ac84f56f8181b4f
Matías Eduardo Allende Pino <matias.allende.p@mail.pucv.cl>
2018-03-31 21:03:17 -0300

    comando 'choose' agregado, elige entre 2 o más parámetros separados por un espacio aleatoriamente

---
+++ commands/choose.js
@@ -0,0 +1,32 @@
+exports.run = (client, message, args, sender, perms) => {
+    const moment = require('moment');
+    const chalk = require('chalk');
+    const settings = require('../settings.json');
+    if (args.length < 2){
+        message.channel.send(`Por favor ingresa al menos 2 parámetros para elegir, revisa \`${settings.prefix}help choose\` para más información`)
+        console.log(chalk.bgRed('[Choose]') + ' '+ sender.username + '@' + message.channel.name + ` sin argumentos mínimos [${moment().format('YYYY-MM-DD HH:mm:ss')}]`);
+        return;
+    }
+    let indexChoose = Math.floor(Math.random() *args.length);
+    message.channel.send(`Yo eligo`)
+        .then(msg => {
+            msg.edit(`Yo eligo...`)
+                .then(msg => {
+                    msg.edit(`Yo eligo... \`${args[indexChoose]}\``)
+        })
+    });
+    console.log(chalk.bgRed('[Choose]') + ' '+ sender.username + '@' + message.channel.name + ` Resultado: ${args[indexChoose]} [${moment().format('YYYY-MM-DD HH:mm:ss')}]`);
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['elige', 'escoge', 'selecciona'],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'choose',
+    description: 'Elige aleatoriamente una opción de los argumentos recibidos separados por un espacio (Min. 2)',
+    usage: 'choose <opcion1> <opcion2> [opcion3]...'
+};
