fa49c33b2b3db22eff7c68de5089cc57b26d1daf
Matías Eduardo Allende Pino <matias.allende.p@mail.pucv.cl>
2018-04-05 05:17:41 -0300

    'choose' ahora trabaja con argumentos separados por comas

--- commands/choose.js
+++ commands/choose.js
@@ -2,12 +2,15 @@ exports.run = (client, message, args, sender, perms) => {
     const moment = require('moment');
     const chalk = require('chalk');
     const settings = require('../settings.json');
-    if (args.length < 2){
+
+    let argString = args.join(" ");
+    let elect = argString.split(",");
+    if (elect.length < 2){
         message.channel.send(`Por favor ingresa al menos 2 parámetros para elegir, revisa \`${settings.prefix}help choose\` para más información`)
         console.log(chalk.bgRed('[Choose]') + ' '+ sender.username + '@' + message.channel.name + ` sin argumentos mínimos [${moment().format('YYYY-MM-DD HH:mm:ss')}]`);
         return;
     }
-    let lowerArgs = args.map(v => v.toLowerCase());
+    let lowerArgs = elect.map(v => v.toLowerCase());
     if (lowerArgs.includes('monika')){
         message.channel.send("J̴̙͌ȕ̶̮͜s̴̗̳̐ť̸̊ͅM̸͔̍ǒ̵̗̬n̵̙̙̈́i̴̛̺̱̐k̵͕̟͊å̸͇̭ J̴̙͌ȕ̶̮͜s̴̗̳̐ť̸̊ͅM̸͔̍ǒ̵̗̬n̵̙̙̈́i̴̛̺̱̐k̵͕̟͊å̸͇̭ J̴̙͌ȕ̶̮͜s̴̗̳̐ť̸̊ͅM̸͔̍ǒ̵̗̬n̵̙̙̈́i̴̛̺̱̐k̵͕̟͊å̸͇̭ J̴̙͌ȕ̶̮͜s̴̗̳̐ť̸̊ͅM̸͔̍ǒ̵̗̬n̵̙̙̈́i̴̛̺̱̐k̵͕̟͊å̸͇̭ J̴̙͌ȕ̶̮͜s̴̗̳̐ť̸̊ͅM̸͔̍ǒ̵̗̬n̵̙̙̈́i̴̛̺̱̐k̵͕̟͊å̸͇̭ J̴̙͌ȕ̶̮͜s̴̗̳̐ť̸̊ͅM̸͔̍ǒ̵̗̬n̵̙̙̈́i̴̛̺̱̐k̵͕̟͊å̸͇̭ J̴̙͌ȕ̶̮͜s̴̗̳̐ť̸̊ͅM̸͔̍ǒ̵̗̬n̵̙̙̈́i̴̛̺̱̐k̵͕̟͊å̸͇̭ J̴̙͌ȕ̶̮͜s̴̗̳̐ť̸̊ͅM̸͔̍ǒ̵̗̬n̵̙̙̈́i̴̛̺̱̐k̵͕̟͊å̸͇̭");
         message.channel.send("JUST");
@@ -15,16 +18,15 @@ exports.run = (client, message, args, sender, perms) => {
         console.log(chalk.bgRed('[Choose]') + ' '+ sender.username + '@' + message.channel.name + ` MONIKA [${moment().format('YYYY-MM-DD HH:mm:ss')}]`);
         return;
     }
-
-    let indexChoose = Math.floor(Math.random() *args.length);
+    let indexChoose = Math.floor(Math.random() *elect.length);
     message.channel.send(`Yo eligo`)
         .then(msg => {
             msg.edit(`Yo eligo...`)
                 .then(msg => {
-                    msg.edit(`Yo eligo... \`${args[indexChoose]}\``)
+                    msg.edit(`Yo eligo... \`${elect[indexChoose]}\``)
         })
     });
-    console.log(chalk.bgRed('[Choose]') + ' '+ sender.username + '@' + message.channel.name + ` Resultado: ${args[indexChoose]} [${moment().format('YYYY-MM-DD HH:mm:ss')}]`);
+    console.log(chalk.bgRed('[Choose]') + ' '+ sender.username + '@' + message.channel.name + ` Resultado: ${elect[indexChoose]} [${moment().format('YYYY-MM-DD HH:mm:ss')}]`);
 };
 
 exports.conf = {
@@ -36,6 +38,6 @@ exports.conf = {
 
 exports.help = {
     name: 'choose',
-    description: 'Elige aleatoriamente una opción de los argumentos recibidos separados por un espacio (Min. 2)',
-    usage: 'choose <opcion1> <opcion2> [opcion3]...'
+    description: 'Elige aleatoriamente una opción de los argumentos recibidos separados por coma `,` (Min. 2)',
+    usage: 'choose <opcion1>, <opcion2>, [opcion3]...'
 };
