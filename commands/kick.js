5a93c0e1bb54d93bd86c01e3b5f1d8fcff4c2850
LAPTOP-KVVFB5AS\fortr <matias.allende.p@mail.pucv.cl>
2018-06-11 18:24:16 -0400

    '-kick' arreglado

--- commands/kick.js
+++ commands/kick.js
@@ -9,18 +9,19 @@ exports.run = (client, message, args, sender, perms) => {
     if (user === sender) return message.channel.send('Kick fallido: No puedes kickearte a ti mismo');
     if (user.id === client.user.id) return message.channel.send('Kick fallido: Tranquilo crack, yo no me voy asi de fácil');
     const modlog = message.guild.channels.find('name', 'mod-log');
+    var kickNombre = user.user.tag
     user.kick().then((user) => {
-        console.log(chalk.bgBlackBright('[Kick]') + ' ' + sender.username + '@' + message.channel.name + ` Usuario: ${user.tag}@${message.guild.id}:${message.guild.name} - [${moment().format('YYYY-MM-DD HH:mm:ss')}]`);
+        console.log(chalk.bgBlackBright('[Kick]') + ' ' + sender.username + '@' + message.channel.name + ` Usuario: ${kickNombre}@${message.guild.id}:${message.guild.name} - [${moment().format('YYYY-MM-DD HH:mm:ss')}]`);
         if(modlog){
             const reason = args.splice(1, args.length).join(' ') || `Razón no especificada`;
-            message.channel.send(`Usuario ${user.tag} kickeado!`);
+            message.channel.send(`Usuario ${kickNombre} kickeado!`);
             const embed = new RichEmbed()
                 .setColor(0x00AE86)
                 .setTimestamp()
-                .setDescription(`**Acción:** Kick\n**Objetivo:** ${user.tag}\n**Moderador:** ${message.author.tag}\n**Razón:** ${reason}`)
+                .setDescription(`**Acción:** Kick\n**Objetivo:** ${kickNombre}\n**Moderador:** ${message.author.tag}\n**Razón:** ${reason}`)
             return client.channels.get(modlog.id).send({embed});
         }
-        message.channel.send(`Usuario ${user.tag} kickeado!`);
+        message.channel.send(`Usuario ${kickNombre} kickeado!`);
         message.channel.send('Para un aviso más detallado por favor crea un canal llamado `mod-log`');
     }).catch(() => {
         message.channel.send('Ups, algo salió mal...\nTal vez no tengo permisos, `' + settings.prefix + 'support` para el servidor de ayuda :thermometer_face:');
@@ -34,7 +35,7 @@ exports.conf = {
     guildOnly: false,
     aliases: ['kickea2'],
     permLevel: 2,
-    category: 'guild'
+  category: 'guild'
 };
 
 exports.help = {
